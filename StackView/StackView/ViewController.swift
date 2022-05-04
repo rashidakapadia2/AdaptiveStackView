da4257730ce6d4123e92ef03f854ab60f77a8129
rashidakapadia2 <rashidakapadia2@gmail.com>
2022-05-04 18:45:41 +0530

    Stack View in adaptive layout

---
+++ StackView/StackView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by Neosoft on 01/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var stackView: UIStackView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
